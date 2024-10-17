734478739144effaf12161e6c066fbc3466b28f5
RishabhhG <hydrarishabh9@gmail.com>
2024-10-17 15:10:29 +0530

    Added API CLIENT

---
+++ src/utils/constant.js
@@ -0,0 +1,22 @@
+export const HOST = import.meta.env.VITE_SERVER_URL;
+export const AUTH_ROUTE = "api/auth";
+
+export const SIGNUP_ROUTE = `${AUTH_ROUTE}/`;
+export const LOGIN_ROUTE = `${AUTH_ROUTE}/`;
+export const LOG_OUT = `${AUTH_ROUTE}/`;
+
+
+export const CLIENT_ROUTE = "api/client";
+
+export const SHIPMENT_ROUTE = "api/shipment";
+
+export const TRUCK_ROUTE = "api/truck";
+
+export const DRIVER_ROUTE = "api/driver";
+
+export const BILLING_ROUTE = "api/billing";
+
+
+
+
+
